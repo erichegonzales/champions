151df804119b995ec73f7f4bec04b2c005978ba0
Eriche Gonzales <erichelouise@gmail.com>
2022-07-01 16:56:21 -0400

    Added proficiencies list

--- index.js
+++ index.js
@@ -2,11 +2,22 @@
 // https://www.dnd5eapi.co/api/classes
 
 const classSelector = document.getElementById('class-select')
+const charImg = document.getElementById('char-img')
+const profList = document.getElementById('proficiencies-list')
 const baseImgUrl = 'https://www.dndbeyond.com/attachments/thumbnails/0/'
 const portraitMap = {
+    'barbarian': '679/400/417/c3barbarianintro.png',
+    'bard': '684/400/406/c3bardintro.png',
+    'cleric': '687/380/437/c3clericintro.png',
+    'druid': '693/400/399/c3druidintro.png',
     'fighter': '697/400/475/c3fighterintro.png',
+    'paladin': '701/400/473/c3paladinintro.png',
+    'monk': '700/400/490/c3monkintro.png',
+    'ranger': '707/400/444/c3rangerintro.png',
+    'rogue': '709/375/480/c3rogueintro.png',
+    'sorcerer': '712/400/517/c3sorcererintro.png',
     'wizard': '717/400/484/c3wizardintro.png',
-    'barbarian': '679/400/417/c3barbarianintro.png'
+    'warlock': '716/400/512/c3warlockintro.png'
 }
 
 let request = async () => {
@@ -21,15 +32,21 @@ let request = async () => {
         classSelector.append(option)
     })
 
-    classSelector.addEventListener('change', (e) => {
+    classSelector.addEventListener('change', async (e) => {
         document.getElementById('selected-class').innerText = e.target.value
-        let img = document.createElement('img')
-        img.id = 'char-img'
-        img.src = `${baseImgUrl}${portraitMap[e.target.value.toLowerCase()]}`
+        let src = `${baseImgUrl}${portraitMap[e.target.value.toLowerCase()]}`
+        charImg.src = src
+        charImg.classList.remove('hidden')
+        req = await fetch(`https://www.dnd5eapi.co/api/classes/${e.target.value.toLowerCase()}`)
+        res = await req.json()
+        profList.innerHTML = ''
+        res.proficiencies.forEach((prof) => {
+            let li = document.createElement('li')
+            li.innerText = prof.name
+            profList.append(li)
+        })
+        profList.classList.remove('hidden')
         console.log(e.target.value)
-        img.setAttribute('width', '400px')
-        img.setAttribute('height', '400px')
-        document.body.append(img)
     })
 }
 
